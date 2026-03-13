7ff024d55f7bda5bcb0c03bc53ee9b9ab6cfbbf5
master <master@email.com>
2026-03-13 13:20:00 +0900

    進捗管理の１ページの表示行数変更

--- WorkOps/Components/Pages/TPlanActualPages/Index.razor.cs
+++ WorkOps/Components/Pages/TPlanActualPages/Index.razor.cs
@@ -52,7 +52,7 @@ public partial class Index
     }
 
     private List<InputModel>? InputModels;
-    private readonly PaginationState pagination = new() { ItemsPerPage = 20 };
+    private readonly PaginationState pagination = new() { ItemsPerPage = 30 };
     private List<ApplicationUser> Users = [];
 
     /// <summary>
